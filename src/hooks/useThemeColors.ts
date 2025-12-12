3b08725a87dbb5813bf6c9121b692141a9a505a1
qossai03 <qossai.douba@icloud.com>
2025-12-12 16:19:25 +0100

    Keep only admin folder in this branch

--- src/hooks/useThemeColors.ts
+++
@@ -1,8 +0,0 @@
-import { useTheme } from '../contexts/ThemeContext';
-import { lightTheme, darkTheme, Theme } from '../components/theme/themes';
-
-export function useThemeColors(): Theme {
-  const { isDarkMode } = useTheme();
-  return isDarkMode ? darkTheme : lightTheme;
-}
-
